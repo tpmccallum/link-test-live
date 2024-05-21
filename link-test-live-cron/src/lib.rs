83c6681c4955365beb05ce237defb6489035784b
tpmccallum <tim.mccallum@fermyon.com>
2024-05-21 17:09:58 +1000

    Updating as we go
    Signed-off-by: tpmccallum <tim.mccallum@fermyon.com>

--- link-test-live-cron/src/lib.rs
+++ link-test-live-cron/src/lib.rs
@@ -1,32 +1,31 @@
 use serde_json::Value;
-use spin_cron_sdk::{cron_component, Error, Metadata};
+use spin_cron_sdk::{cron_component, Metadata};
+use spin_sdk::{
+    http::{Method, Request, Response},
+};
 
 #[cron_component]
-fn handle_cron_event(metadata: Metadata) -> Result<(), Error> {
-    use serde_json::Value;
-    use spin_cron_sdk::{cron_component, Metadata};
-    use spin_sdk::{
-        http::{Method, Request, Response},
-        key_value::Store,
-    };
-
-    #[cron_component]
-    async fn handle_cron_event(metadata: Metadata) -> anyhow::Result<()> {
-        // Build request to the Cloud API
-        let request = Request::builder()
-            .method(Method::Get)
-            // need to add json body here that bundles up the links_per_batch and the user's api key 
-            .uri("https://link-test-live-cloud.fermyon.app")
-            .build();
-        // The response will be as follows {"url": "https://developer.fermyon.com}
-        // Await the response from the API, then store the response
-        let response: Response = spin_sdk::http::send(request).await?;
-        // Extract the JSON data from the body of the response
-        let json_data: Value = serde_json::from_slice(&response.body())?;
-        // Create a new instance of the struct (an index variable) that stores the timestamp and uvi
-        let url = json_data["url"].to_string().as_str();
-        // Check the URL and create a new json object to send to the cloud
-        println!("Processed {:?} at timestamp: [{}]", url, index.get_timestamp());
-        Ok(())
-    }
+async fn handle_cron_event(metadata: Metadata) -> anyhow::Result<()> {
+    // Build request to the Cloud API
+    let request = Request::builder()
+        .method(Method::Get)
+        // need to add json body here that bundles up the links_per_batch and the user's api key 
+        .uri("https://link-test-live-cloud.fermyon.app")
+        .build();
+    // The response will be as follows {"url": "https://developer.fermyon.com}
+    // Await the response from the API, then store the response
+    let response: Response = spin_sdk::http::send(request).await?;
+    // Extract the JSON data from the body of the response
+    let json_data: Value = serde_json::from_slice(&response.body())?;
+    // Create a new instance of the struct (an index variable) that stores the timestamp and uvi
+    let url = json_data["url"].to_string().as_str();
+    // Create a new request that checks the URL (obviously this will be checking many in the future not just one)
+    let request = Request::builder()
+    .method(Method::Get)
+    // need to add json body here that bundles up the links_per_batch and the user's api key 
+    .uri(url)
+    .build();
+    // Check the URL and create a new json object to send to the cloud
+    println!("Processed {:?} at timestamp: [{}]", url, metadata.timestamp);
+    Ok(())
 }
