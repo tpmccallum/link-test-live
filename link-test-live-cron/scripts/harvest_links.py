4d9b4b830fc452360f70ebde506856ad69d2f67b
tpmccallum <tim.mccallum@fermyon.com>
2024-06-03 20:34:58 +1000

    Add script to harvest links
    Signed-off-by: tpmccallum <tim.mccallum@fermyon.com>

--- link-test-live-cron/scripts/harvest_links.py
+++ link-test-live-cron/scripts/harvest_links.py
@@ -25,6 +25,7 @@ for website_sitemap_loc_element in website_sitemap_loc_elements:
 print("Number of page to process is {}\n First page to process is {} and the last page to process is {}".format(len(website_page_urls), website_page_urls[0], website_page_urls[len(website_page_urls) - 1]))
 
 page_url_dict = {}
+page_int = 1
 for one_page in website_page_urls:
     print(f"Processing {one_page}")
     page_url_dict = {}
@@ -32,36 +33,86 @@ for one_page in website_page_urls:
     response = requests.get(one_page)
     if response.status_code == 200:
         if one_page.startswith("http"):
+            #if page_int == 2:
+            #    continue
             soup = BeautifulSoup(response.content, 'html.parser')
             links = soup.find_all('a')
             urls = [link.get('href') for link in links]
             urls = [url for url in urls if url is not None and (url.startswith('http') or url.startswith('https'))]
             urls = list(set(urls))
             current_time_epoch = int(time.time())
-            sql_insert_statement = "INSERT OR IGNORE INTO URLTable (single_url, last_checked_epoch, status) VALUES\n"
+            sql_insert_statement = "INSERT OR IGNORE INTO URLTable (single_url, last_checked_epoch, response_code, status_code) VALUES\n"
             values = [
-                f"('{url}', {current_pointer_epoch}, 'pending')"
+                f"('{url}', {current_pointer_epoch}, NULL, 'pending')"
                 for url in urls
                 for current_pointer_epoch in (current_time_epoch,)  # Fixed current time for all URLs
             ]
-    sql_insert_statement += ",\n".join(values) + ";"
-    with open('insert_urls.sql', 'w') as file:
-        file.write(sql_insert_statement)
-
+            sql_insert_statement += ",\n".join(values) + ";"
+            filename = 'insert_urls_' + str(page_int) + '.sql'
+            page_int += 1
+            with open(filename, 'w') as file:
+                file.write(sql_insert_statement)
 
 """ 
 This script will generate a SQL file with the following content:
 
-INSERT OR IGNORE INTO URLTable (single_url, last_checked_epoch, status) VALUES
-('https://developer.fermyon.com/bartholomew/contributing-docs', 1717123750, 'pending'),
-('https://github.com/fermyon/developer', 1717123750, 'pending'),
-('https://www.fermyon.com/', 1717123750, 'pending'),
-('https://rdmd.readme.io/docs/code-blocks#language-support', 1717123750, 'pending'),
-('https://developer.fermyon.com/bartholomew/quickstart', 1717123750, 'pending'),
-('https://developer.fermyon.com/bartholomew/scripting', 1717123750, 'pending') 
 
 Loading this SQL file into the database is done in the following way:
 
 For the local app `spin up --sqlite @insert_urls.sql`
 For the Cloud app `spin cloud sqlite execute -d mydb @insert_urls.sql`
+
+If fragmenting the files, into a SQLite page per file, then here is a bash script to load all the SQL files into the Cloud API database:
+
+#!/bin/bash
+
+# Loop through each file that starts with 'insert_urls_' and ends with '.sql'
+for sql_file in insert_urls_*.sql
+do
+  echo "Executing ${sql_file}..."
+  # Execute the command with the current SQL file
+  spin cloud sqlite execute -d mydb @"${sql_file}"
+  
+  # Check if the command was successful
+  if [ $? -eq 0 ]; then
+    echo "${sql_file} executed successfully."
+  else
+    echo "Failed to execute ${sql_file}."
+  fi
+
+  # Pause for 2 seconds
+  sleep 2
+done
+
+
+Here is a bash script to insert the sql locally:
+
+#!/bin/bash
+
+# Loop through each file that starts with 'insert_urls_' and ends with '.sql'
+for sql_file in insert_urls_*.sql
+do
+  echo "Executing ${sql_file}..."
+  # Execute the command with the current SQL file
+  spin cloud sqlite execute -d mydb @"${sql_file}"
+  
+  # Check if the command was successful
+  if [ $? -eq 0 ]; then
+    echo "${sql_file} executed successfully."
+  else
+    echo "Failed to execute ${sql_file}."
+  fi
+
+  # Pause for 2 seconds
+  sleep 2
+done
+
+You can then check the database out using the local CLI
+
+sqlite3 .spin/sqlite_db.db
+sqlite> .tables
+
+
 """
+
+
--- link-test-live-cron/src/lib.rs
+++ link-test-live-cron/src/lib.rs
@@ -24,114 +24,97 @@ async fn handle_cron_event(metadata: Metadata) -> anyhow::Result<()> {
     let cloud_url_string: String = cloud_url.url_to_string();
     println!("Checking for API key, please wait ...");
     let store = Store::open_default()?;
-    // TODO REMOVE THIS SET TO NO LINE
-    store.set("busy", "no".as_bytes())?;
-    //panic!("Just resetting the busy flag to no for testing purposes ...");
     match store.get("api_key")? {
         Some(value) => {
             match store.get("busy")? {
-                Some(busy_value) => {
-                    match std::str::from_utf8(&busy_value) {
-                        Ok(busy_str) => {
-                            match busy_str {
-                                "yes" => {
-                                    println!("Still busy checking the last batch of links. Will try again later ...");
-                                }
-                                "no" => {
-                                    let mut json_result = json!({
-                                        "urls": [],
-                                    });
-                                    println!("Fetching new batch of links to check ...");
-                                    store.set("busy", "yes".as_bytes())?;
+                Some(busy_value) => match std::str::from_utf8(&busy_value) {
+                    Ok(busy_str) => match busy_str {
+                        "yes" => {
+                            println!("Still busy checking the last batch of links. Will try again later ...");
+                        }
+                        "no" => {
+                            let mut json_result = json!({
+                                "urls": [],
+                            });
+                            println!("Fetching new batch of links to check ...");
+                            store.set("busy", "yes".as_bytes())?;
 
-                                    let string_value = String::from_utf8(value)?;
-                                    println!(
-                                        "Using API Key {:?}",
-                                        Some(string_value.to_string().trim_matches('"'))
-                                    );
-                                    let links_per_batch = variables::get("links")?;
-                                    let host_whitelist = variables::get("hosts")?;
-                                    let mut json_request_data = json!({
-                                        "api_key": string_value.to_string().trim_matches('"'),
-                                        "links_per_batch": links_per_batch.as_str(),
-                                        "allowed_outbound_hosts": host_whitelist.as_str()
-                                    });
-                                    let request = Request::builder()
-                                        .method(Method::Post)
-                                        .uri(cloud_url_string.clone())
-                                        .body(serde_json::to_vec(&json_request_data)?)
-                                        .build();
-                                    let response: Response = spin_sdk::http::send(request).await?;
-                                    let json_data: serde_json::Value =
-                                        serde_json::from_slice(&response.body())?;
-                                    let mut json_result = json!({
-                                        "results": [],
-                                    });
-                                    if let Some(urls) = json_data["urls"].as_array() {
-                                        for url in urls {
-                                            if let Some(url_str) = url.as_str() {
-                                                println!("Processing: {}", url_str);
-                                                let request = Request::builder()
-                                                    .method(Method::Get)
-                                                    .uri(url.as_str().unwrap())
-                                                    .build();
-                                                let response: Response =
-                                                    spin_sdk::http::send(request).await?;
-                                                let status_code = response.status();
-                                                let url_entry =
-                                                    json!({"url": url, "status_code": status_code});
-                                                json_result["results"]
-                                                    .as_array_mut()
-                                                    .unwrap()
-                                                    .push(url_entry);
-                                            } else {
-                                                println!("No 'url' string found in the response.");
-                                            }
-                                        }
-                                        if json_request_data.get("results").is_none() {
-                                            json_request_data["results"] = json!([]);
-                                        }
-                                        json_request_data["results"]
-                                            .as_array_mut()
-                                            .unwrap()
-                                            .push(json_result.clone());
-                                        println!("All checked\n\n: {:?}", json_result);
+                            let string_value = String::from_utf8(value)?;
+                            println!(
+                                "Using API Key {:?}",
+                                Some(string_value.to_string().trim_matches('"'))
+                            );
+                            let links_per_batch = variables::get("links")?;
+                            let host_whitelist = variables::get("hosts")?;
+                            let mut json_request_data = json!({
+                                "api_key": string_value.to_string().trim_matches('"'),
+                                "links_per_batch": links_per_batch.as_str(),
+                                "allowed_outbound_hosts": host_whitelist.as_str()
+                            });
+                            let request = Request::builder()
+                                .method(Method::Post)
+                                .uri(cloud_url_string.clone())
+                                .body(serde_json::to_vec(&json_request_data)?)
+                                .build();
+                            let response: Response = spin_sdk::http::send(request).await?;
+                            let json_data: serde_json::Value =
+                                serde_json::from_slice(&response.body())?;
+                            let mut json_result = json!({
+                                "results": [],
+                            });
+                            if let Some(urls) = json_data["urls"].as_array() {
+                                for url in urls {
+                                    if let Some(url_str) = url.as_str() {
+                                        println!("Processing: {}", url_str);
                                         let request = Request::builder()
-                                            .method(Method::Post)
-                                            .uri(cloud_url_string.clone())
-                                            .body(serde_json::to_vec(&json_result)?)
+                                            .method(Method::Get)
+                                            .uri(url.as_str().unwrap())
                                             .build();
                                         let response: Response =
                                             spin_sdk::http::send(request).await?;
-                                        //check response code
-                                        let status_code: u16 = *response.status();
-                                        if status_code == 200 {
-                                            println!("Points awarded successfully!");
-                                            store.set("busy", "no".as_bytes())?;
-                                        } else {
-                                            println!(
-                                                "Failed to award points! Something went wrong ..."
-                                            );
-                                        }
+                                        let response_code = response.status();
+                                        let url_entry = json!({"url": url, "response_code": response_code, "status_code": "processed"});
+                                        json_result["results"]
+                                            .as_array_mut()
+                                            .unwrap()
+                                            .push(url_entry);
                                     } else {
-                                        println!("No 'urls' array found in the response.");
+                                        println!("No 'url' string found in the response.");
                                     }
                                 }
-                                _ => {
+                                json_request_data["results"] = json_result;
+                                let request = Request::builder()
+                                    .method(Method::Post)
+                                    .uri(cloud_url_string.clone())
+                                    .body(serde_json::to_vec(&json_request_data)?)
+                                    .build();
+                                let response: Response = spin_sdk::http::send(request).await?;
+                                let response_code: u16 = *response.status();
+                                if response_code == 200 {
+                                    let response_body = std::str::from_utf8(&response.body())?;
                                     store.set("busy", "no".as_bytes())?;
-                                    println!("Initializing, please wait! Will fetch links to check soon ...");
+                                } else {
+                                    println!("Failed to award points! Something went wrong ...");
+                                    store.set("busy", "no".as_bytes())?;
                                 }
+                            } else {
+                                println!("No 'urls' array found in the response.");
+                                store.set("busy", "no".as_bytes())?;
                             }
                         }
-                        Err(e) => {
-                            println!("Failed to convert busy value to string: {:?}", e);
-                            // Add your error handling logic here
+                        _ => {
+                            store.set("busy", "no".as_bytes())?;
+                            println!(
+                                "Initializing, please wait! Will fetch links to check soon ..."
+                            );
                         }
+                    },
+                    Err(e) => {
+                        println!("Failed to convert busy value to string: {:?}", e);
                     }
-                }
+                },
                 None => {
                     println!("The 'busy' key is not present in the store.");
-                    // Add your logic for the None case here
                 }
             }
 
@@ -150,6 +133,7 @@ async fn handle_cron_event(metadata: Metadata) -> anyhow::Result<()> {
             let api_key_trimmed = api_key.trim_matches('"');
             println!("New API Key: {:?}", api_key_trimmed);
             store.set("api_key", api_key_trimmed.as_bytes())?;
+            store.set("busy", "no".as_bytes())?;
             Ok(())
         }
     }
